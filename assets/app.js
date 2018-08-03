d04639b3a3e6866b9ceb2da59b2c38335de354a5
Christopher Carlson <c.m.carlson13@gmail.com>
2018-08-03 16:30:21 -0600

    Pretty much functional

--- assets/app.js
+++ assets/app.js
@@ -1,73 +1,114 @@
 // Characters
 
-var person1 = {
-  name: 'Guy Dudeson',
-  health: 150,
-  healthFull: 150,
-  hits: 0,
+var characters = {
+  person1: {
+    name: 'Guy Dudeson',
+    health: 150,
+    healthFull: 150,
+    hits: 0,
+    items: [],
+  },
+  person2: {
+    name: 'Dude Guyson',
+    health: 120,
+    healthFull: 120,
+    hits: 0,
+    items: [],
+  },
 }
 
-var person2 = {
-  name: 'Dude Guyson',
-  health: 120,
-  healthFull: 120,
-  hits: 0,
-}
 
 // Items
 
-var fire = {
-  name: 'Fire',
-  modifier: 2,
-  description: 'IT BURNS!',
+
+var items = {
+  fire: {
+    name: 'Fire',
+    modifier: 2,
+    description: 'IT BURNS!',
+  },
+  ice: {
+    name: 'Ice',
+    modifier: 1,
+    description: 'ITS COLD!',
+  },
+}
+
+let selectedCharacter = characters.person1
+
+function character1() {
+  selectedCharacter = characters.person1;
+  draw();
+}
+
+function character2() {
+  selectedCharacter = characters.person2;
+  draw();
+}
+
+function addMods() {
+  let total = 0;
+  for (let i = 0; i < selectedCharacter.items.length; i++) {
+    total += selectedCharacter.items[i].modifier;
+  }
+  return total;
 }
 
 function draw() {
-  if (person.health <= 0) {
-    person.health = 0;
+  if (selectedCharacter.health <= 0) {
+    selectedCharacter.health = 0;
   }
-  document.getElementById("health").innerHTML = person.health;
-  document.getElementById("hits").innerHTML = person.hits;
-  document.getElementById("name").innerHTML = person.name;
+  document.getElementById("health").innerHTML = selectedCharacter.health;
+  document.getElementById("hits").innerHTML = selectedCharacter.hits;
+  document.getElementById("name").innerHTML = selectedCharacter.name;
 }
 
 draw();
 
 function reset() {
-  person.health = person.healthFull;
-  person.hits = 0;
+  selectedCharacter.health = selectedCharacter.healthFull;
+  selectedCharacter.hits = 0;
+  selectedCharacter.items = [];
   draw();
 }
 
 function slap() {
-  if (person.health <= 0) {
-    person.hits++;
+  if (selectedCharacter.health <= 0) {
+    selectedCharacter.hits++;
     draw();
   } else {
-    person.health--;
-    person.hits++;
+    selectedCharacter.health -= 1 + addMods();
+    selectedCharacter.hits++;
     draw()
   };
 }
 
 function punch() {
-  if (person.health <= 0) {
-    person.hits++;
+  if (selectedCharacter.health <= 0) {
+    selectedCharacter.hits++;
     draw();
   } else {
-    person.health -= 5;
-    person.hits++;
+    selectedCharacter.health -= 5;
+    selectedCharacter.hits++;
     draw();
   }
 }
 function kick() {
-  if (person.health <= 0) {
-    // person.health = 0;
-    person.hits++;
+  if (selectedCharacter.health <= 0) {
+    // selectedCharacter.health = 0;
+    selectedCharacter.hits++;
     draw();
   } else {
-    person.health -= 10;
-    person.hits++;
+    selectedCharacter.health -= 10;
+    selectedCharacter.hits++;
     draw();
   }
+}
+
+function giveFire() {
+  selectedCharacter.items.push(items.fire);
+}
+
+function giveIce() {
+  selectedCharacter.items.push(items.ice);
 }
