54bd83b505b60ddb78246d97c0a5eff0701c629b
Christopher Carlson <c.m.carlson13@gmail.com>
2018-08-05 08:54:32 -0600

    game mode

--- assets/app.js
+++ assets/app.js
@@ -26,6 +26,14 @@ var characters = {
     img: '<img src="assets/brute.png" alt="">',
     items: [],
   },
+  player: {
+    name: 'Player',
+    health: 120,
+    healthFull: 120,
+    hits: 0,
+    img: '<img src="assets/lose.png" alt="">',
+    items: [],
+  },
 }
 let selectedCharacter = characters.person1
 
@@ -56,11 +64,16 @@ function draw() {
     document.getElementById("health").innerHTML = "0";
     document.getElementById("name").innerHTML = "Enemy Defeated!";
     document.getElementById("enemyImg").innerHTML = " "
+  } else if (characters.player.health <= 0) {
+    document.getElementById("playerHealth").innerHTML = 0;
+    document.getElementById("enemyImg").innerHTML = characters.player.img;
+    document.getElementById("health").innerHTML = '';
   } else {
     document.getElementById("health").innerHTML = selectedCharacter.health;
     document.getElementById("hits").innerHTML = selectedCharacter.hits;
     document.getElementById("name").innerHTML = selectedCharacter.name;
     document.getElementById('enemyImg').innerHTML = selectedCharacter.img;
+    document.getElementById("playerHealth").innerHTML = characters.player.health;
   };
 }
 
@@ -89,10 +102,14 @@ function addMods() {
     total += selectedCharacter.items[i].modifier;
   };
   return total;
+  enemyAttack();
 };
 
 function reset() {
-  selectedCharacter.health = selectedCharacter.healthFull;
+  characters.person1.health = characters.person1.healthFull;
+  characters.person2.health = characters.person2.healthFull;
+  characters.person3.health = characters.person3.healthFull;
+  characters.player.health = characters.player.healthFull;
   selectedCharacter.hits = 0;
   selectedCharacter.items = [];
   draw();
@@ -105,7 +122,8 @@ function slap() {
   } else {
     selectedCharacter.health -= 1 + addMods();
     selectedCharacter.hits++;
-    draw()
+    draw();
+    enemyAttack();
   };
 }
 
@@ -117,6 +135,7 @@ function punch() {
     selectedCharacter.health -= 5 + addMods();
     selectedCharacter.hits++;
     draw();
+    enemyAttack();
   }
 }
 function kick() {
@@ -127,17 +146,39 @@ function kick() {
     selectedCharacter.health -= 10 + addMods();
     selectedCharacter.hits++;
     draw();
+    enemyAttack();
   }
 }
 
 function giveFire() {
   selectedCharacter.items.push(items.fire);
+  enemyAttack();
 }
 
 function giveIce() {
   selectedCharacter.items.push(items.ice);
+  enemyAttack();
 }
 
 function giveWater() {
   selectedCharacter.items.push(items.water);
+  enemyAttack();
+}
+
+function enemyAttack() {
+  if (selectedCharacter == characters.character1) {
+    characters.player.health -= Math.floor(Math.random() * 5);
+    draw();
+  } else if (selectedCharacter == characters.person2) {
+    characters.player.health -= Math.floor(Math.random() * 10);
+    draw();
+  } else {
+    characters.player.health -= Math.floor(Math.random() * 15);
+    draw();
+  }
+}
+
+function heal() {
+  characters.player.health += Math.floor(Math.random() * 100);
+  enemyAttack();
 }
